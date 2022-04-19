af9908f88366553312e3c58acd030b49d8354a1c
RemLampa <rem@remlampa.com>
2022-04-19 11:31:13 +0800

    :lipstick: reorder opinion story avatar

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+
+import { QUERIES } from "../../constants";
 
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
@@ -17,6 +19,19 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  gap: 8px;
+
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: column;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    flex-direction: row-reverse;
+  }
 `;
 
 const Avatar = styled.img`
